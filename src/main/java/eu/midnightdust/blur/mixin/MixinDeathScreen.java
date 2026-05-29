c062cbde145d08e36a5138554466ce64f121e64c
amiralimollaei <me.amiralimollaei@gmail.com>
2026-05-29 23:56:59 +0330

    revert MixinDeathScreen

--- src/main/java/eu/midnightdust/blur/mixin/MixinDeathScreen.java
+++ src/main/java/eu/midnightdust/blur/mixin/MixinDeathScreen.java
@@ -2,7 +2,9 @@ package eu.midnightdust.blur.mixin;
 
 import eu.midnightdust.blur.Blur;
 import eu.midnightdust.blur.config.BlurConfig;
+import it.unimi.dsi.fastutil.booleans.BooleanConsumer;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.screens.ConfirmScreen;
 import net.minecraft.client.gui.screens.DeathScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
@@ -11,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin({DeathScreen.class, DeathScreen.TitleConfirmScreen.class})
+@Mixin(DeathScreen.class)
 public class MixinDeathScreen extends Screen {
     protected MixinDeathScreen(Component title) {
         super(title);
@@ -31,4 +33,26 @@ public class MixinDeathScreen extends Screen {
     }
     //~}
     //~}
+
+    @Mixin(DeathScreen.TitleConfirmScreen.class)
+    public static class MixinTitleConfirmScreen extends ConfirmScreen {
+        public MixinTitleConfirmScreen(BooleanConsumer callback, Component title, Component message) {
+            super(callback, title, message);
+        }
+
+        // forces death screen to also render a blurred background
+        //~ if >= 26.1 'render' -> 'extract' {
+        //~ if >= 26.1 'GuiGraphics' -> 'GuiGraphicsExtractor' {
+        @Inject(
+                method = "extractBackground",
+                at = @At(
+                        value = "TAIL"
+                )
+        )
+        private void blur$extractBackground(GuiGraphicsExtractor context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+            if (BlurConfig.blurDeathScreen && Blur.canBlur(context)) this.extractBlurredBackground(/*? if > 1.21.5 {*/ context /*?} else if <= 1.21.1 {*/ /*delta *//*?}*/);
+        }
+        //~}
+        //~}
+    }
 }
